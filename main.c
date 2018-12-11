23db605f99b4f487d375474c0a5d6326e749e683
neo.cheng <neo77neo44@gmail.com>
2018-12-11 23:01:56 +0800

    modify csma sch

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <windows.h>
+#include <signal.h>
+#include <sched.h>
 
 typedef unsigned char vc_uint8;
 typedef unsigned int vc_uint16;
@@ -17,34 +19,65 @@ vc_uint32 NodeSeed[100];
 pthread_t Node[100];
 pthread_t CSMA_SCH;
 
-vc_uint32 totalMs = 200;
-vc_uint32 raminMs = 200;
+vc_uint32 totalMs = 2000;
+vc_uint32 raminMs = 2000;
 vc_uint32 currentMs = 0;
 vc_uint32 pktSize = 15;
 vc_uint8 pktSending = 0;
-void* NodeSch(int nodeSeed) {
+vc_uint32 waitForPktSend=0;
+vc_uint32 NodeBackoffTime[100];
+vc_uint32 NodeSendingStatus[100];
+vc_uint32 visionTime=10;
+vc_uint16 csmaSchTick[7]={0,25,50,75,575,1075,1575};
+void* NodeSch(int nodeIdx) {
     int a;
-    srand(nodeSeed);
+    int backoff=0;
+    srand(nodeIdx);
     while(currentMs != totalMs){
-        a=(rand()%100)+1;
-        printf("nodeSeed:%lu, %d,%d\r\n",currentMs,nodeSeed, a);
-        Sleep(50);
+        NodeBackoffTime[nodeIdx] = (rand()%100)+1;
+        if(NodeBackoffTime[nodeIdx] >= currentMs)
+        {
+            if(pktSending){
+                backoff = (rand()%100)+1;
+                //printf("%lu,Collision!\r\n",currentMs);
+                NodeBackoffTime[nodeIdx] = currentMs + backoff;
+            }else{
+                //printf("%lu,Sending!\r\n",currentMs);
+                NodeSendingStatus[nodeIdx] = 1;
+            }
+        }
+        Sleep(visionTime);
     }
 }
 void csma_sch(void* data) {
     while(1){
         if(currentMs!=totalMs){
-
+            if(currentMs>=csmaSchTick[0] && currentMs < csmaSchTick[1]){
+                    printf("%d->%s\r\n",currentMs,"BCN_A");
+            }else if(currentMs>=csmaSchTick[1] && currentMs < csmaSchTick[2]){
+                    printf("%d->%s\r\n",currentMs,"BCN_B");
+            }else if(currentMs>=csmaSchTick[2] && currentMs < csmaSchTick[3]){
+                    printf("%d->%s\r\n",currentMs,"BCN_C");
+            }else if(currentMs>=csmaSchTick[3] && currentMs < csmaSchTick[4]){
+                    printf("%d->%s\r\n",currentMs,"CSMA_A");
+            }else if(currentMs>=csmaSchTick[4] && currentMs < csmaSchTick[5]){
+                    printf("%d->%s\r\n",currentMs,"CSMA_B");
+            }else if(currentMs>=csmaSchTick[5] && currentMs < csmaSchTick[6]){
+                    printf("%d->%s\r\n",currentMs,"CSMA_C");
+            }else{
+                    printf("%d->%s\r\n",currentMs,"IDLE");
+            }
         }else{
             pthread_exit(NULL);
         }
         currentMs++;
-        Sleep(50);
+        currentMs%=2000;
+        Sleep(visionTime);
     }
 }
 int main()
 {
-    int node_num = 4;
+    int node_num = 2;
     int i;
     for(i=0;i<node_num;i++){
         pthread_create(&Node[i], NULL, NodeSch, i);
@@ -55,4 +88,5 @@ int main()
     }
     pthread_join(CSMA_SCH, NULL);
     return 0;
+
 }
